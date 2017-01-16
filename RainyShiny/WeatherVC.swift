f306d9b34159718bbea8f8dc1653dbec869d6c4d
nguyenhungnq <NguyenHung@antonio.local>
2017-01-16 11:11:02 +0700

    Greating the interface with IBOutlets

---
+++ RainyShiny/WeatherVC.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherVC.swift
+//  RainyShiny
+//
+//  Created by NguyenHung on 1/14/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTemplabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+
+}
+
